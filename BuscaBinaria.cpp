e175327b1e64246104f323865367339d9c85aaba
natalete <natsalete14@gmail.com>
2024-11-13 12:27:21 -0300

    feat: adição de novos arquivos

--- BuscaBinaria.cpp
+++ BuscaBinaria.cpp
@@ -7,6 +7,34 @@
 
 using namespace std;
 
+/*Ler o arquivo nomes.txt em anexo.
+
+Utilizando a busca binária, ler um nome e retornar todos os nomes que se encaixam com a busca no início da palavra.
+
+Por exemplo:
+
+Adalberto Estevez
+Adelia Rosmaninho
+Adelina Paredes
+Adelio Clementino
+Afonso Cantanhede
+Afonso Herrera
+Bruna Adenisia
+
+Se buscar pelas iniciais Ade, sua busca deve trazer "Adelia Rosmaninho", "Adelina Paredes", "Adelio Clementino".
+
+
+Ignorar letras maiúsculas e minúsculas na hora de ler.
+
+Funções úteis:
+
+https://cplusplus.com/reference/cstring/strstr/
+
+https://cplusplus.com/reference/cctype/toupper/
+
+https://cplusplus.com/reference/cctype/tolower
+*/
+
 // Função para converter uma string para minúsculas (case insensitive)
 string toLowerCase(const string& str) {
     string result = str;
